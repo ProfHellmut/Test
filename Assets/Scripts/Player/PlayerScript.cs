2d295ce95669ba76fc2ea92112ecfb97a5b1ecb4
Markus Nordin <markus.nordin@falun.ntig.se>
2018-05-29 10:35:09 +0200

    123
    123

---
+++ Assets/Scripts/Player/PlayerScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour {
+
+    public float speed = 10f;
+    public float jumpHeight = 5f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float movement = Input.GetAxis("horizontal");
+	}
+
+}
